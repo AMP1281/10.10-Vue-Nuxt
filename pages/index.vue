24b6940a4423d996438ab2c8c66d6970f4593a8e
Anna Montserrat <amp1281@gmail.com>
2021-08-02 18:58:28 +0200

    ok

---
+++ pages/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <h1>Welcome nuxt</h1>
+</template>
+
+<script>
+export default {
+
+  //Este objeto ayuda al SEO,añadimos meta información
+  head(){
+    return{
+      title:'Exercici Final amb Nuxt | Home',
+      meta:[
+        {
+        hid:"description",
+        name:"description",
+        content:"Nuxt",
+        }
+      ],
+      htmlAttrs:{
+        lang:'en'
+      }
+    }
+  }
+
+}
+</script>
